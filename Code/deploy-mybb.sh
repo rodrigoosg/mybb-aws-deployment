d4887e92c006da932235efd5dc87b491fb0abaa6
Rodrigo Guimarães <rodrigo@agileoperations.com.br>
2017-01-08 23:22:56 -0200

    Changed directory structure to match requirements; Changed scripts accordingly;

---
+++ Code/deploy-mybb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# Instalation script for MYBB
+#
+
+# Configuration variables
+CONFIG="./Code"
+MYBB_PACKAGE="./Code/mybb_1809_with_theme.zip"
+DEPLOYMENT_DIR="/var/www/html"
+
+# Assert that apache deployment dir is clean
+rm -rf "$DEPLOYMENT_DIR"/*
+
+# Unzip MYBB installation files inside apache deployment dir
+unzip "$MYBB_PACKAGE"
+
+# Copy extracted files into installation directory
+cp -r ./Upload/* "$DEPLOYMENT_DIR"/
+
+# Replace variables in prepared configuration files
+sed -e "s/MYBB_ADMINEMAIL/${MYBB_ADMINEMAIL}/g" -e "s/MYBB_DOMAINNAME/${MYBB_DOMAINNAME}/g" "${CONFIG}/settings.php" > "${DEPLOYMENT_DIR}/inc/settings.php"
+
+sed -e "s/MYBB_DBNAME/${MYBB_DBNAME}/g" -e "s/MYBB_DBUSERNAME/${MYBB_DBUSERNAME}/g" -e "s/MYBB_DBPASSWORD/${MYBB_DBPASSWORD}/g" \
+    -e "s/MYBB_DBHOSTNAME/${MYBB_DBHOSTNAME}/g" -e "s/MYBB_DBPORT/${MYBB_DBPORT}/g" "${CONFIG}/config.php" > "${DEPLOYMENT_DIR}/inc/config.php"
+
+# Configure database with ADMIN and DOMAIN variables
+sed -e "s/MYBB_ADMINEMAIL/${MYBB_ADMINEMAIL}/g" -e "s/MYBB_DOMAINNAME/${MYBB_DOMAINNAME}/g" "${CONFIG}/mybb.sql" 
+
+# Load configured DUMP file into RDS database
+mysql --user="$MYBB_DBUSERNAME" --password="$MYBB_DBPASSWORD" --host="$MYBB_DBHOSTNAME" --port="$MYBB_DBPORT" --database="$MYBB_DBNAME" < "${CONFIG}/mybb.sql" || echo "Schema Already Exists!"
+
+# Set proper ownership and permissions.
+cd "$DEPLOYMENT_DIR"
+chmod 666 inc/config.php inc/settings.php
+chmod 666 inc/languages/english/*.php inc/languages/english/admin/*.php
+chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/
+chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/ admin/backups/
+
+# Remove install directory
+rm -rf /var/www/html/install
